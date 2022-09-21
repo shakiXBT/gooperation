08ee42d695bdb1d02e8678e377a0e19ed4bec88f
shakiXBT <shakiXBT@protonmail.com>
2022-09-21 22:50:28 +0200

    chore: setup

---
+++ src/Gooperation.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+contract Gooperation {
+
+    address public immutable artGobblers;
+
+    constructor(address _artGobblers) {
+        artGobblers = _artGobblers;
+    }
+
+}
