4f616390d3e3e831091fc52575b56949dffb025a
shakiXBT <shakiXBT@protonmail.com>
2022-09-24 20:34:09 +0200

    chore: deposit checks, tests

--- src/Gooperation.sol
+++ src/Gooperation.sol
@@ -8,17 +8,52 @@ import {ERC721TokenReceiver} from "solmate/tokens/ERC721.sol";
 contract Gooperation is ERC721TokenReceiver {
 
     ArtGobblers public immutable artGobblers;
+    Goo public immutable goo;
 
     /// @dev gobblerOwnerships[userAddress][gobblerId] == true
     mapping(address => mapping(uint256 => bool)) public gobblerOwnerships;
 
-    /// @param _artGobblers ArtGobblers contract address
-    constructor(ArtGobblers _artGobblers) {
-        artGobblers = _artGobblers;
+    mapping(address => uint256) public getUserMultiplier;
+
+    mapping(address => uint256) public gooOwnerships;
+
+    bool public wonAuction;
+
+    // ERRORS
+
+    error Unauthorized();
+    error DepositsDisabled();
+
+    modifier ownsGobbler(uint256 _gobblerId) {
+        if (!gobblerOwnerships[msg.sender][_gobblerId]) revert Unauthorized();
+
+        _;
     }
 
-    function withdrawGobblerTo(address to, uint256 gobblerId) public {
-        require(gobblerOwnerships[msg.sender][gobblerId], "UNAUTHORIZED WITHDRAWAL");
+    modifier onlyBeforeAuction() {
+        if (wonAuction) revert DepositsDisabled();
+
+        _;
+    }
+
+    modifier onlyBelowAuctionStartingPrice() {
+        (uint128 auctionprice,) = artGobblers.legendaryGobblerAuctionData();
+        if (artGobblers.balanceOf(address(this)) > auctionprice) {
+            revert DepositsDisabled();
+        }
+
+        _;
+    }
+
+    /// @notice Sets the addresses of relevant contracts
+    /// @param _artGobblers Address of the ArtGobblers contract
+    /// @param _goo Address of the Goo contract
+    constructor(ArtGobblers _artGobblers, Goo _goo) {
+        artGobblers = _artGobblers;
+        goo = _goo;
+    }
+
+    function withdrawGobblerTo(address to, uint256 gobblerId) public ownsGobbler(gobblerId) {
         artGobblers.safeTransferFrom(address(this), to, gobblerId);
     }
 
@@ -28,12 +63,39 @@ contract Gooperation is ERC721TokenReceiver {
 
     function bidLegendaryGobbler() public {}
 
-    function onERC721Received(address, address to, uint256 gobblerId, bytes memory) public virtual override returns (bytes4) {
-        gobblerOwnerships[to][gobblerId] = true;
+    function onERC721Received(address from, address, uint256 gobblerId, bytes memory) public virtual override onlyBeforeAuction() onlyBelowAuctionStartingPrice() returns (bytes4) {
+        // update user ownership
+        gobblerOwnerships[from][gobblerId] = true;
+        // update user multiplier
+        getUserMultiplier[from] += artGobblers.getGobblerEmissionMultiple(gobblerId);
+
         return this.onERC721Received.selector;
     }
 
     function getGobblerOwnership(address owner, uint256 gobblerId) public view returns (bool) {
         return gobblerOwnerships[owner][gobblerId];
     }
+
+    /// @notice requires approval
+    function depositGoo(uint256 _amount) public {
+        goo.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    /*
+    function depositGoo() {
+        // check if approved for transfer or revert? maybe dont need since it reverts by itself
+     
+
+        // burn goo erc20 to add virtual goo balance
+        artgobblers.addGoo()
+    }
+
+    function withdrawGoo() {
+        // burn virtual goo to withdraw goo balance
+        // will need to keep track of goo belonging to a user
+        artgobblers.removeGoo()
+    }
+
+
+    */
 }
--- test/Gooperation.t.sol
+++ test/Gooperation.t.sol
@@ -18,7 +18,6 @@ import {VRFCoordinatorMock} from "chainlink/v0.8/mocks/VRFCoordinatorMock.sol";
 contract GooperationTest is DSTestPlus {
 
     // GOOPERATION VARS
-
     
     Gooperation public gooperation;
 
@@ -46,7 +45,7 @@ contract GooperationTest is DSTestPlus {
     function setUp() public {
 
         gobblerSetup();
-        gooperation = new Gooperation(gobblers);
+        gooperation = new Gooperation(gobblers, goo);
     }
 
     function gobblerSetup() public {
@@ -111,6 +110,10 @@ contract GooperationTest is DSTestPlus {
         // verify gooperation ownership
         assertEq(gobblers.ownerOf(1), address(gooperation));
         assertEq(gobblers.balanceOf(address(gooperation)), 1);
+
+        // verify gobbler ownership inside gooperation
+        assert(gooperation.getGobblerOwnership(user, 1));
+
     }
     
     /// @dev check Gobbler withdrawal from Gooperation
@@ -149,12 +152,111 @@ contract GooperationTest is DSTestPlus {
         // deposit
         vm.prank(user);
         gobblers.safeTransferFrom(user, address(gooperation), 1);
-
         // withdraw
         vm.prank(user1);
-        vm.expectRevert(bytes("UNAUTHORIZED WITHDRAWAL"));
+        vm.expectRevert(Gooperation.Unauthorized.selector);
         gooperation.withdrawGobblerTo(user1, 1);
         
         assertEq(gobblers.balanceOf(user1), 0);
+    }
+
+    function testGooIssuance() public {
+        address user = users[0];
+        bytes32[] memory proof;
+        vm.prank(user);
+        gobblers.claimGobbler(proof);
+
+        vm.prank(address(gobblers));
+        goo.mintForGobblers(user, 1000000);
+
+        vm.prank(user);
+        gobblers.addGoo(1000000);
+
+        // warp for reveal
+        vm.warp(block.timestamp + 1 days);
+        setRandomnessAndReveal(1, "seed");
+
+        emit log_named_uint("user multipler:", gobblers.getUserEmissionMultiple(user));
+        emit log_named_uint("gobbler multipler:", gobblers.getGobblerEmissionMultiple(1));
+
+        emit log_named_uint("initial goo balance erc20", goo.balanceOf(user));
+        emit log_named_uint("initial goo balance virtual", gobblers.gooBalance(user));
+        emit log_named_uint("initial timestamp: ", block.timestamp); 
+
+        // warp to accrue goo
+        vm.warp(block.timestamp + 100);
+
+        emit log_named_uint("final goo balance erc20", goo.balanceOf(user));
+        emit log_named_uint("final goo balance virtual", gobblers.gooBalance(user));
+        emit log_named_uint("final timestamp: ", block.timestamp); 
+
+        // TODO actual test w/ assert, for now just used to debug contracts
+    }
+
+    function testDepositDisabled() public {
+        address user = users[0];
+        mintGobblerToAddress(user, 70);
+
+        (uint128 auctionprice,) = gobblers.legendaryGobblerAuctionData();
+        emit log_named_uint("legendary gobbler price:", auctionprice);
+
+        for (uint256 i = 0; i != auctionprice; ++i) {
+            vm.prank(user);
+            gobblers.safeTransferFrom(user, address(gooperation), i+1);
+        }
+
+        vm.expectRevert(Gooperation.DepositsDisabled.selector);
+        vm.prank(user);
+        gobblers.safeTransferFrom(user, address(gooperation), auctionprice + 1);
+        // do the same but with auction won bool
+    }
+
+    /*
+    function testDepositGoo() public {
+        address user = users[0];
+        bytes32[] memory proof;
+        vm.prank(user);
+        gobblers.claimGobbler(proof);
+
+        vm.prank(address(gobblers));
+        goo.mintForGobblers(user, 1000000);
+
+        // deposit goo into gooperation
+        vm.prank(user);
+        gooperation.depositGoo(1000000);
+    }
+    */
+
+    // HELPERS
+
+    function mintGobblerToAddress(address addr, uint256 num) internal {
+        for (uint256 i = 0; i < num; ++i) {
+            vm.startPrank(address(gobblers));
+            goo.mintForGobblers(addr, gobblers.gobblerPrice());
+            vm.stopPrank();
+
+            uint256 gobblersOwnedBefore = gobblers.balanceOf(addr);
+
+            vm.prank(addr);
+            gobblers.mintFromGoo(type(uint256).max, false);
+
+            assertEq(gobblers.balanceOf(addr), gobblersOwnedBefore + 1);
+        }
+    }
+
+    /// @notice Call back vrf with randomness and reveal gobblers.
+    function setRandomnessAndReveal(uint256 numReveal, string memory seed) internal {
+        bytes32 requestId = gobblers.requestRandomSeed();
+        uint256 randomness = uint256(keccak256(abi.encodePacked(seed)));
+        // call back from coordinator
+        vrfCoordinator.callBackWithRandomness(requestId, randomness, address(randProvider));
+        gobblers.revealGobblers(numReveal);
+    }
+
+    function mintNextLegendary(address addr) internal {
+        uint256[] memory id;
+        mintGobblerToAddress(addr, gobblers.LEGENDARY_AUCTION_INTERVAL() * 2);
+        vm.prank(addr);
+        gobblers.mintLegendaryGobbler(id);
     }
 }
